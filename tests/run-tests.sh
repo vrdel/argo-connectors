7d78ba392e76ac56f9ed1ee432113eee554e672b
Daniel Vrcic <daniel.vrcic@gmail.com>
2017-06-23 12:21:28 +0200

    clean created links and files needed for tests

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,23 +4,37 @@ sitepack=$(python -c 'import sys; \
            sitedirs=[p for p in sys.path if p.endswith("site-packages")]; \
            print sitedirs[0]')
 
+declare -a files
+
 if [[ ! -L $sitepack/argo_egi_connectors ]]
 then
     ln -s $PWD/modules $sitepack/argo_egi_connectors
+    files[${#files[@]}]="$sitepack/argo_egi_connectors"
 fi
 
-(cd bin && \
+pushd bin/ > /dev/null
+
 for f in [a-z]*.py
 do
     link=${f//-/_} 
     if [[ ! -L $link ]]
     then
         ln -s $f $link
+        files[${#files[@]}]="bin/$link"
     fi
-done && \
+done
+
 if [[ ! -e __init__.py ]]
 then 
     touch __init__.py
-fi)
+    files[${#files[@]}]="bin/__init__.py"
+fi
 
-coverage run --source=tests -m unittest2 discover tests && coverage xml  
+popd > /dev/null
+
+coverage run --source=tests -m unittest2 discover tests && coverage xml
+
+for f in ${files[@]}
+do
+    rm -f $f
+done
