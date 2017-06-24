20584107c7e28bb39182d15e1456b7917deb345a
Daniel Vrcic <daniel.vrcic@gmail.com>
2017-06-24 18:38:56 +0200

    test for topology group endpoints AMS publishing

--- tests/test_output.py
+++ tests/test_output.py
@@ -80,3 +80,60 @@ class TopologyAms(unittest.TestCase):
             ret, excep = self.amspublish.send(self.globopts['AvroSchemasTopologyGroupOfGroups'.lower()],
                                             'group_groups', datestamp().replace('_', '-'), group_groups)
             self.assertTrue(ret)
+
+    def testGroupEndpoints(self):
+        group_endpoints = [{'group': u'100IT',
+                            'hostname': u'occi-api.100percentit.com',
+                            'service': u'eu.egi.cloud.vm-management.occi',
+                            'tags': {'monitored': '1',
+                                     'production': '1',
+                                     'scope': 'EGI'},
+                            'type': 'SITES'},
+                           {'group': u'100IT',
+                            'hostname': u'egi-cloud-accounting.100percentit.com',
+                            'service': u'eu.egi.cloud.accounting',
+                            'tags': {'monitored': '1',
+                                     'production': '1',
+                                     'scope': 'EGI'},
+                            'type': 'SITES'},
+                           {'group': u'100IT',
+                            'hostname': u'occi-api.100percentit.com',
+                            'service': u'eu.egi.cloud.information.bdii',
+                            'tags': {'monitored': '1',
+                                     'production': '1',
+                                     'scope': 'EGI'},
+                            'type': 'SITES'}]
+
+        @urlmatch(**self.get_topic_urlmatch)
+        def get_topic_mock(url, request):
+            # Return the details of a topic in json format
+            return response(200, '{"name": "/projects/EGI/topics/TOPIC"}', None, None, 5, request)
+
+        @urlmatch(**self.publish_topic_urlmatch)
+        def publish_bulk_mock(url, request):
+            assert url.path == "/v1/projects/EGI/topics/TOPIC:publish"
+            # Check request produced by ams client
+            req_body = json.loads(request.body)
+            self.assertEqual(req_body["messages"][0]["data"], "ClNJVEVTCjEwMElUPmV1LmVnaS5jbG91ZC52bS1tYW5hZ2VtZW50Lm9jY2kyb2NjaS1hcGkuMTAwcGVyY2VudGl0LmNvbQIGCnNjb3BlBkVHSRRwcm9kdWN0aW9uAjESbW9uaXRvcmVkAjEA")
+            self.assertEqual(req_body["messages"][0]["attributes"]["type"], "group_endpoints")
+            self.assertEqual(req_body["messages"][0]["attributes"]["report"], "EGI_Critical")
+            self.assertEqual(req_body["messages"][0]["attributes"]["partition_date"], datestamp().replace('_', '-'))
+
+            self.assertEqual(req_body["messages"][1]["data"], "ClNJVEVTCjEwMElULmV1LmVnaS5jbG91ZC5hY2NvdW50aW5nSmVnaS1jbG91ZC1hY2NvdW50aW5nLjEwMHBlcmNlbnRpdC5jb20CBgpzY29wZQZFR0kUcHJvZHVjdGlvbgIxEm1vbml0b3JlZAIxAA==")
+            self.assertEqual(req_body["messages"][1]["attributes"]["type"], "group_endpoints")
+            self.assertEqual(req_body["messages"][1]["attributes"]["report"], "EGI_Critical")
+            self.assertEqual(req_body["messages"][1]["attributes"]["partition_date"], datestamp().replace('_', '-'))
+
+            self.assertEqual(req_body["messages"][2]["data"], "ClNJVEVTCjEwMElUOmV1LmVnaS5jbG91ZC5pbmZvcm1hdGlvbi5iZGlpMm9jY2ktYXBpLjEwMHBlcmNlbnRpdC5jb20CBgpzY29wZQZFR0kUcHJvZHVjdGlvbgIxEm1vbml0b3JlZAIxAA==")
+            self.assertEqual(req_body["messages"][2]["attributes"]["type"], "group_endpoints")
+            self.assertEqual(req_body["messages"][2]["attributes"]["report"], "EGI_Critical")
+            self.assertEqual(req_body["messages"][2]["attributes"]["partition_date"], datestamp().replace('_', '-'))
+
+            return '{"msgIds": ["1", "2", "3"]}'
+
+
+        with HTTMock(get_topic_mock, publish_bulk_mock):
+            ret, excep = self.amspublish.send(self.globopts['AvroSchemasTopologyGroupOfEndpoints'.lower()],
+                                            'group_endpoints', datestamp().replace('_', '-'), group_endpoints)
+            self.assertTrue(ret)
+
