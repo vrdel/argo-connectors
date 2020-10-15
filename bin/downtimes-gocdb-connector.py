fcec745ebb8bce8782e49cde9b346f1fbcc4da59
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-10-15 16:59:11 +0200

    remove customer jobs loop

--- bin/downtimes-gocdb-connector.py
+++ bin/downtimes-gocdb-connector.py
@@ -156,7 +156,7 @@ def main():
     confcust.parse()
     confcust.make_dirstruct()
     confcust.make_dirstruct(globopts['InputStateSaveDir'.lower()])
-    feeds = confcust.get_mapfeedjobs(sys.argv[0], deffeed='https://goc.egi.eu/gocdbpi/')
+    logger.customer = confcust.get_custname()
 
     if len(args.date) == 0:
         print(parser.print_help())
@@ -173,97 +173,84 @@ def main():
         logger.error(e)
         raise SystemExit(1)
 
-    j = 0
-    for feed, jobcust in feeds.items():
-        customers = set(map(lambda jc: confcust.get_custname(jc[1]), jobcust))
-        customers = customers.pop() if len(customers) == 1 else '({0})'.format(','.join(customers))
-        sjobs = set(map(lambda jc: jc[0], jobcust))
-        jobs = list(sjobs)[0] if len(sjobs) == 1 else '({0})'.format(','.join(sjobs))
-        logger.job = jobs
-        logger.customer = customers
-        uidjob = confcust.pass_uidserviceendpoints(sjobs)
+    uidjob = confcust.pass_uidserviceendpoints(sjobs)
 
-        auth_custopts = confcust.get_authopts(feed, jobcust)
-        auth_opts = cglob.merge_opts(auth_custopts, 'authentication')
-        auth_complete, missing = cglob.is_complete(auth_opts, 'authentication')
-        if not auth_complete:
-            logger.error('Customer:%s Jobs:%s %s options incomplete, missing %s'
-                        % (logger.customer, logger.job, 'authentication',
-                            ''.join(missing)))
-            continue
+    auth_custopts = confcust.get_authopts()
+    auth_opts = cglob.merge_opts(auth_custopts, 'authentication')
+    auth_complete, missing = cglob.is_complete(auth_opts, 'authentication')
+    if not auth_complete:
+        logger.error('Customer:%s %s options incomplete, missing %s'
+                    % (logger.customer, 'authentication',
+                        ''.join(missing)))
+        raise SystemExit(1)
 
-        # we don't have multiple tenant definitions in one
-        # customer file so we can safely assume one tenant/customer
-        cust = jobcust[0][1]
-        write_empty = confcust.send_empty(sys.argv[0], cust)
+    # we don't have multiple tenant definitions in one
+    # customer file so we can safely assume one tenant/customer
+    write_empty = confcust.send_empty(sys.argv[0], cust)
 
-        # do fetch only once
-        if all_same(uidjob):
-            gocdb = GOCDBReader(feed, auth_opts, uidjob[j])
-            if not write_empty:
-                dts = gocdb.getDowntimes(start, end)
-            else:
-                dts = []
-                gocdb.state = True
+    gocdb = GOCDBReader(feed, auth_opts, uidjob[j])
+    if not write_empty:
+        dts = gocdb.getDowntimes(start, end)
+    else:
+        dts = []
+        gocdb.state = True
 
-        for job, cust in jobcust:
-            # fetch for every job because of different TopoUIDServiceEndpoints
-            # setting for each job
-            if not all_same(uidjob):
-                gocdb = GOCDBReader(feed, auth_opts, uidjob[j])
-                if not write_empty:
-                    dts = gocdb.getDowntimes(start, end)
-                else:
-                    dts = []
-                    gocdb.state = True
+    # fetch for every job because of different TopoUIDServiceEndpoints
+    # setting for each job
+    if not all_same(uidjob):
+        gocdb = GOCDBReader(feed, auth_opts, uidjob[j])
+        if not write_empty:
+            dts = gocdb.getDowntimes(start, end)
+        else:
+            dts = []
+            gocdb.state = True
 
-            jobdir = confcust.get_fulldir(cust, job)
-            jobstatedir = confcust.get_fullstatedir(globopts['InputStateSaveDir'.lower()], cust, job)
+    jobdir = confcust.get_fulldir(cust, job)
+    jobstatedir = confcust.get_fullstatedir(globopts['InputStateSaveDir'.lower()], cust, job)
 
-            logger.customer = confcust.get_custname(cust)
-            logger.job = job
+    logger.customer = confcust.get_custname(cust)
 
-            webapi_custopts = confcust.get_webapiopts(cust)
-            webapi_opts = cglob.merge_opts(webapi_custopts, 'webapi')
-            webapi_complete, missopt = cglob.is_complete(webapi_opts, 'webapi')
-            if not webapi_complete:
-                logger.error('Customer:%s Job:%s %s options incomplete, missing %s' % (logger.customer, job, 'webapi', ' '.join(missopt)))
-                continue
+    webapi_custopts = confcust.get_webapiopts(cust)
+    webapi_opts = cglob.merge_opts(webapi_custopts, 'webapi')
+    webapi_complete, missopt = cglob.is_complete(webapi_opts, 'webapi')
+    if not webapi_complete:
+        logger.error('Customer:%s Job:%s %s options incomplete, missing %s' % (logger.customer, job, 'webapi', ' '.join(missopt)))
+        raise SystemExit(1)
 
-            output.write_state(sys.argv[0], jobstatedir, gocdb.state, globopts['InputStateDays'.lower()], timestamp)
+    output.write_state(sys.argv[0], jobstatedir, gocdb.state, globopts['InputStateDays'.lower()], timestamp)
 
-            if not gocdb.state:
-                continue
+    if not gocdb.state:
+        raise SystemExit(1)
 
-            if eval(globopts['GeneralPublishWebAPI'.lower()]):
-                webapi = output.WebAPI(sys.argv[0],
-                                       webapi_opts['webapihost'],
-                                       webapi_opts['webapitoken'],
-                                       logger,
-                                       int(globopts['ConnectionRetry'.lower()]),
-                                       int(globopts['ConnectionTimeout'.lower()]),
-                                       int(globopts['ConnectionSleepRetry'.lower()]),
-                                       date=args.date[0],
-                                       report=confcust.get_jobdir(job))
-                webapi.send(dts)
+    if eval(globopts['GeneralPublishWebAPI'.lower()]):
+        webapi = output.WebAPI(sys.argv[0],
+                               webapi_opts['webapihost'],
+                               webapi_opts['webapitoken'],
+                               logger,
+                               int(globopts['ConnectionRetry'.lower()]),
+                               int(globopts['ConnectionTimeout'.lower()]),
+                               int(globopts['ConnectionSleepRetry'.lower()]),
+                               date=args.date[0],
+                               report=confcust.get_jobdir(job))
+        webapi.send(dts)
 
-            if eval(globopts['GeneralWriteAvro'.lower()]):
-                filename = filename_date(logger, globopts['OutputDowntimes'.lower()], jobdir, stamp=timestamp)
-                avro = output.AvroWriter(globopts['AvroSchemasDowntimes'.lower()], filename)
-                ret, excep = avro.write(dts)
-                if not ret:
-                    logger.error('Customer:%s Job:%s %s' % (logger.customer, logger.job, repr(excep)))
-                    raise SystemExit(1)
+    if eval(globopts['GeneralWriteAvro'.lower()]):
+        filename = filename_date(logger, globopts['OutputDowntimes'.lower()], jobdir, stamp=timestamp)
+        avro = output.AvroWriter(globopts['AvroSchemasDowntimes'.lower()], filename)
+        ret, excep = avro.write(dts)
+        if not ret:
+            logger.error('Customer:%s Job:%s %s' % (logger.customer, logger.job, repr(excep)))
+            raise SystemExit(1)
 
-            j += 1
+    j += 1
 
-        if gocdb.state:
-            custs = set([cust for job, cust in jobcust])
-            for cust in custs:
-                jobs = [job for job, lcust in jobcust if cust == lcust]
-                logger.info('Customer:%s Jobs:%s Fetched Date:%s Endpoints:%d' % (confcust.get_custname(cust),
-                                                                                  jobs[0] if len(jobs) == 1 else '({0})'.format(','.join(jobs)),
-                                                                                  args.date[0], len(dts)))
+    if gocdb.state:
+        custs = set([cust for job, cust in jobcust])
+        for cust in custs:
+            jobs = [job for job, lcust in jobcust if cust == lcust]
+            logger.info('Customer:%s Jobs:%s Fetched Date:%s Endpoints:%d' % (confcust.get_custname(cust),
+                                                                                jobs[0] if len(jobs) == 1 else '({0})'.format(','.join(jobs)),
+                                                                                args.date[0], len(dts)))
 
 
 if __name__ == '__main__':
