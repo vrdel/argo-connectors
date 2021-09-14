96128565d4cd2c3d728813ad47f2408359ccaade
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-09-14 12:46:40 +0200

    underscore instead of dot

--- modules/parse/eosc.py
+++ modules/parse/eosc.py
@@ -62,7 +62,7 @@ class ParseEoscTopo(object):
                 tmp_dict['hostname'] = '{1}_{0}'.format(entity['Service Unique ID'], self._construct_fqdn(info_url))
             else:
                 tmp_dict['hostname'] = self._construct_fqdn(entity['URL'])
-            tmp_dict['tags'] = {'scope': self.scope, 'monitored': '1', 'info.URL': info_url}
+            tmp_dict['tags'] = {'scope': self.scope, 'monitored': '1', 'info_URL': info_url}
 
             groups.append(tmp_dict)
 
