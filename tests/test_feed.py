4c604a6aa5ce0f3835ce03ea2783d95af2e01a7d
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-09-21 17:47:04 +0200

    set up of test for parsing service endpoints

--- tests/test_feed.py
+++ tests/test_feed.py
@@ -6,11 +6,16 @@ from argo_egi_connectors.parse.gocdb_topology import ParseServiceGroups, ParseSe
 
 logger = Logger('test_feed.py')
 
-class ParseSitesTest(unittest.TestCase):
-    def setUp(self):
-        self.parse_sites = ParseSites(logger, data, 'FOO', uid=True, pass_extensions=True)
 
-    def test_parsesites(self):
+class ParseServiceEndpointsTest(unittest.TestCase):
+    def setUp(self):
+        with open('tests/sample-service_endpoint.xml') as feed_file:
+            content = feed_file.read()
+        logger.customer = 'CUSTOMERFOO'
+        parse_service_endpoints = ParseServiceEndpoints(logger, content, 'CUSTOMERFOO', uid=True, pass_extensions=True)
+        self.group_endpoints = parse_service_endpoints.get_group_endpoints()
+
+    def test_parseserviceendpoints(self):
         pass
 
 
