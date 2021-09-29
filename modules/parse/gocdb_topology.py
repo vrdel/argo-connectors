96e67e1cee3de1d8b78cd0e99abb565dc6da06c4
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-09-29 20:20:31 +0200

    have common ParseHelpers in parse base

--- modules/parse/gocdb_topology.py
+++ modules/parse/gocdb_topology.py
@@ -1,83 +1,9 @@
-import xml.dom.minidom
-from xml.parsers.expat import ExpatError
-from argo_egi_connectors.tools import filename_date, module_class_name
 from argo_egi_connectors.io.http import ConnectorError
+from argo_egi_connectors.parse.base import ParseHelpers
+from argo_egi_connectors.utils import module_class_name
 
 
-class tools(object):
-    def __init__(self, logger, *args, **kwargs):
-        self.logger = logger
-
-    def _parse_xmltext(self, nodelist):
-        rc = []
-        for node in nodelist:
-            if node.nodeType == node.TEXT_NODE:
-                rc.append(node.data)
-        return ''.join(rc)
-
-    def _parse_extensions(self, extensionsNode):
-        extensions_dict = dict()
-
-        for extension in extensionsNode:
-            if extension.nodeName == 'EXTENSION':
-                key, value = None, None
-                for ext_node in extension.childNodes:
-                    if ext_node.nodeName == 'KEY':
-                        key = ext_node.childNodes[0].nodeValue
-                    if ext_node.nodeName == 'VALUE':
-                        value = ext_node.childNodes[0].nodeValue
-                    if key and value:
-                        extensions_dict.update({key: value})
-
-        return extensions_dict
-
-    def _parse_url_endpoints(self, endpointsNode):
-        endpoints_urls = list()
-
-        for endpoint in endpointsNode:
-            if endpoint.nodeName == 'ENDPOINT':
-                url = None
-                for endpoint_node in endpoint.childNodes:
-                    if endpoint_node.nodeName == 'ENDPOINT_MONITORED':
-                        value = endpoint_node.childNodes[0].nodeValue
-                        if value.lower() == 'y':
-                            for url_node in endpoint.childNodes:
-                                if url_node.nodeName == 'URL' and url_node.childNodes:
-                                    url = url_node.childNodes[0].nodeValue
-                                    endpoints_urls.append(url)
-
-        if endpoints_urls:
-            return ', '.join(endpoints_urls)
-        else:
-            return None
-
-    def _parse_scopes(self, xml_node):
-        scopes = list()
-
-        for elem in xml_node.childNodes:
-            if elem.nodeName == 'SCOPES':
-                for subelem in elem.childNodes:
-                    if subelem.nodeName == 'SCOPE':
-                        scopes.append(subelem.childNodes[0].nodeValue)
-
-        return scopes
-
-    def _parse_xml(self, data):
-        try:
-            return xml.dom.minidom.parseString(data)
-
-        except ExpatError as exc:
-            msg = '{} Customer:{} : Error parsing XML feed - {}'.format(module_class_name(self), self.logger.customer, repr(exc))
-            self.logger.error(msg)
-            raise ConnectorError()
-
-        except Exception as exc:
-            msg = '{} Customer:{} : Error - {}'.format(module_class_name(self), self.logger.customer, repr(exc))
-            self.logger.error(msg)
-            raise exc
-
-
-class ParseSites(tools):
+class ParseSites(ParseHelpers):
     def __init__(self, logger, data, custname, uid=False,
                  pass_extensions=False):
         super().__init__(logger)
@@ -133,7 +59,7 @@ class ParseSites(tools):
         return groupofgroups
 
 
-class ParseServiceEndpoints(tools):
+class ParseServiceEndpoints(ParseHelpers):
     def __init__(self, logger, data, custname, uid=False,
                  pass_extensions=False):
         super().__init__(logger)
@@ -211,7 +137,7 @@ class ParseServiceEndpoints(tools):
         return groupofendpoints
 
 
-class ParseServiceGroups(tools):
+class ParseServiceGroups(ParseHelpers):
     def __init__(self, logger, data, custname, uid=False,
                  pass_extensions=False):
         super().__init__(logger)
