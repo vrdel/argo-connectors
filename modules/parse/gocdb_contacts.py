17fe7348e932bfb5383fb90eb4e01a2c5fc7f7e3
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-09-29 20:23:57 +0200

    initial ParseSiteContacts

--- modules/parse/gocdb_contacts.py
+++ modules/parse/gocdb_contacts.py
@@ -2,9 +2,11 @@ import xml.dom.minidom
 from xml.parsers.expat import ExpatError
 
 from argo_egi_connectors.io.http import ConnectorError
+from argo_egi_connectors.parse.base import ParseHelpers
+from argo_egi_connectors.utils import filename_date, module_class_name
 
 
-class ParseRocContacts(object):
+class ParseRocContacts(ParseHelpers):
     def __init__(self, logger, data, custname):
         super().__init__(logger)
         self.data = data
@@ -16,16 +18,23 @@ class ParseRocContacts(object):
         pass
 
 
-class ParseSiteContacts(object):
+class ParseSiteContacts(ParseHelpers):
     def __init__(self, logger, data, custname):
-        super().__init__(logger)
+        self.logger = logger
         self.data = data
+        self.custname = custname
+        self._parse_data()
 
     def _parse_data(self):
-        pass
+        try:
+            xml_data = self._parse_xml(self.data)
+            sites = xml_data.getElementsByTagName('SITE')
+        except (KeyError, IndexError, TypeError, AttributeError, AssertionError) as exc:
+            self.logger.error(module_class_name(self) + 'Customer:%s : Error parsing - %s' % (self.logger.customer, repr(exc).replace('\'', '').replace('\"', '')))
+            raise exc
 
-    def contacts(self):
-        pass
+    def get_contacts(self):
+        return []
 
 
 class ParseProjectContacts(object):
