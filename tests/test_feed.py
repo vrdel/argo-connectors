c14164b0f4d80ad2fba3d8e20a47fb8b0cc9bb9b
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-09-29 18:53:53 +0200

    move helper functions out of class

--- tests/test_feed.py
+++ tests/test_feed.py
@@ -6,6 +6,24 @@ from argo_egi_connectors.parse.gocdb_topology import ParseServiceGroups, ParseSe
 logger = Logger('test_feed.py')
 CUSTOMER_NAME = 'CUSTOMERFOO'
 
+# Help function - check if any of endpoints contains extensions
+# Used for checking if pass_extensions is working properly
+def endpoints_have_extension(group_endpoints):
+    for gren in group_endpoints:
+        for key in gren['tags'].keys():
+            if key.startswith('info_ext_'):
+                return True
+    return False
+
+# Returns element of group_endpoints with given group_name or None
+def get_group(group_endpoints, group_name):
+    for group in group_endpoints:
+        if group['group'] == group_name:
+            return group
+
+    return None
+
+
 class ParseServiceEndpointsTest(unittest.TestCase):
     def setUp(self):
         with open('sample-service_endpoint.xml') as feed_file:
@@ -14,29 +32,11 @@ class ParseServiceEndpointsTest(unittest.TestCase):
         parse_service_endpoints = ParseServiceEndpoints(logger, self.content, CUSTOMER_NAME)
         self.group_endpoints = parse_service_endpoints.get_group_endpoints()
 
-    # Help function - check if any of endpoints contains extensions
-    # Used for checking if pass_extensions is working properly
-    def endpoints_have_extension(group_endpoints):
-        for gren in group_endpoints:
-            for key in gren['tags'].keys():
-                if key.startswith('info_ext_'):
-                    return True
-        return False
-
-    # Returns element of group_endpoints with given group_name or None
-    def get_group(group_endpoints, group_name):
-        for group in group_endpoints:
-            if group['group'] == group_name:
-                return group
-
-        return None
-
-
     def test_parseserviceendpoints(self):
         self.assertEqual(len(self.group_endpoints), 3) # Parsed correct number of endpoint groups
 
         # Assertions for GSI_LCG2
-        gsi_lcg2 = ParseServiceEndpointsTest.get_group(self.group_endpoints, 'GSI-LCG2')
+        gsi_lcg2 = get_group(self.group_endpoints, 'GSI-LCG2')
         self.assertIsNotNone(gsi_lcg2)
         self.assertEqual(gsi_lcg2['service'], 'CE')
         self.assertEqual(gsi_lcg2['hostname'], 'grid13.gsi.de')
@@ -46,7 +46,7 @@ class ParseServiceEndpointsTest(unittest.TestCase):
         self.assertEqual(gsi_lcg2_tags['production'], '0')
 
         # Assertions for RAL_LCG2
-        ral_lcg2 = ParseServiceEndpointsTest.get_group(self.group_endpoints, 'RAL-LCG2')
+        ral_lcg2 = get_group(self.group_endpoints, 'RAL-LCG2')
         self.assertIsNotNone(ral_lcg2)
         self.assertEqual(ral_lcg2['service'], 'gLite-APEL')
         self.assertEqual(ral_lcg2['hostname'], 'arc-ce01.gridpp.rl.ac.uk')
@@ -56,7 +56,7 @@ class ParseServiceEndpointsTest(unittest.TestCase):
         self.assertEqual(ral_lcg2_tags['production'], '1')
 
         # Assertions for AZ_IFAN
-        az_ifan = ParseServiceEndpointsTest.get_group(self.group_endpoints, 'AZ-IFAN')
+        az_ifan = get_group(self.group_endpoints, 'AZ-IFAN')
         self.assertIsNotNone(az_ifan)
         self.assertEqual(az_ifan['service'], 'CREAM-CE')
         self.assertEqual(az_ifan['hostname'], 'ce.physics.science.az')
@@ -66,25 +66,25 @@ class ParseServiceEndpointsTest(unittest.TestCase):
         self.assertEqual(az_ifan_tags['production'], '1')
 
         # Assert pass_extensions=False is working
-        self.assertFalse(ParseServiceEndpointsTest.endpoints_have_extension(self.group_endpoints))
+        self.assertFalse(endpoints_have_extension(self.group_endpoints))
 
         parse_service_endpoints = ParseServiceEndpoints(logger, self.content, 'CUSTOMERFOO', uid=True, pass_extensions=True)
         # Assert customer name is set properly
         self.assertEqual(parse_service_endpoints.custname, CUSTOMER_NAME)
         group_endpoints = parse_service_endpoints.get_group_endpoints()
-        # Assert pass_extensions=True is working 
-        self.assertTrue(ParseServiceEndpointsTest.endpoints_have_extension(group_endpoints))
+        # Assert pass_extensions=True is working
+        self.assertTrue(endpoints_have_extension(group_endpoints))
 
         # Assert uid=True is working
-        temp_group = ParseServiceEndpointsTest.get_group(group_endpoints, 'GSI-LCG2')
+        temp_group = get_group(group_endpoints, 'GSI-LCG2')
         self.assertIsNotNone(temp_group)
         self.assertEqual(temp_group['hostname'], 'grid13.gsi.de_14G0')
 
-        temp_group = ParseServiceEndpointsTest.get_group(group_endpoints, 'RAL-LCG2')
+        temp_group = get_group(group_endpoints, 'RAL-LCG2')
         self.assertIsNotNone(temp_group)
         self.assertEqual(temp_group['hostname'], 'arc-ce01.gridpp.rl.ac.uk_782G0')
 
-        temp_group = ParseServiceEndpointsTest.get_group(group_endpoints, 'AZ-IFAN')
+        temp_group = get_group(group_endpoints, 'AZ-IFAN')
         self.assertIsNotNone(temp_group)
         self.assertEqual(temp_group['hostname'], 'ce.physics.science.az_1555G0')
 
