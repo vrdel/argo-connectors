9275bb5e6559153598a53f0fe504b5a4f7ea6384
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-10-22 13:32:12 +0200

    use general method for sites parse also

--- modules/parse/gocdb_contacts.py
+++ modules/parse/gocdb_contacts.py
@@ -31,37 +31,7 @@ class ParseSiteContacts(ParseHelpers):
         self._parse_data()
 
     def _parse_data(self):
-        try:
-            data = list()
-            xml_data = self._parse_xml(self.data)
-            sites = xml_data.getElementsByTagName('SITE')
-            for site in sites:
-                if site.nodeName == 'SITE':
-                    emails = list()
-                    for site_node in site.childNodes:
-                        if site_node.nodeName == 'CONTACT':
-                            contact = site_node
-                            interested = ('EMAIL', 'FORENAME', 'SURNAME', 'CERTDN', 'ROLE_NAME')
-                            email, name, surname, certdn, role = self._parse_contact(contact, *interested)
-                            emails.append({
-                                'email': email,
-                                'forename': name,
-                                'surname': surname,
-                                'certdn': certdn,
-                                'role': role
-                            })
-                        if site_node.nodeName == 'SHORT_NAME':
-                            site_name = site_node.childNodes[0].nodeValue
-                data.append({
-                    'name': site_name,
-                    'contacts': emails
-                })
-
-            return data
-
-        except (KeyError, IndexError, TypeError, AttributeError, AssertionError) as exc:
-            self.logger.error(module_class_name(self) + 'Customer:%s : Error parsing - %s' % (self.logger.customer, repr(exc).replace('\'', '').replace('\"', '')))
-            raise exc
+        return self._parse_contacts(self.data, 'SITE', 'CONTACT', 'SHORT_NAME')
 
     def get_contacts(self):
         return self._parse_data()
