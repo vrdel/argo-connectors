1e2e2ded8beb37da8044ba12e6db0f0cb9a36adf
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-10-22 15:58:10 +0200

    add test for roc contacts format

--- tests/test_contactfeed.py
+++ tests/test_contactfeed.py
@@ -1,12 +1,51 @@
 import unittest
 
 from argo_egi_connectors.log import Logger
-from argo_egi_connectors.parse.gocdb_contacts import ParseSiteContacts
+from argo_egi_connectors.parse.gocdb_contacts import ParseSiteContacts, ParseRocContacts
 from argo_egi_connectors.io.http import ConnectorError
 
 
 logger = Logger('test_contactfeed.py')
 CUSTOMER_NAME = 'CUSTOMERFOO'
+
+
+class ParseRocContactTest(unittest.TestCase):
+    def setUp(self):
+        with open('tests/sample-roc_contacts.xml') as feed_file:
+            self.content = feed_file.read()
+        logger.customer = CUSTOMER_NAME
+        parse_roc_contacts = ParseRocContacts(logger, self.content, CUSTOMER_NAME)
+        self.roc_contacts = parse_roc_contacts.get_contacts()
+
+    def test_formatContacts(self):
+        self.assertEqual(self.roc_contacts[0],
+            {
+                'name': 'CERN',
+                'contacts': [
+                    {
+                        'certdn': '/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=Name1/CN=11111/CN=Name1 Surname1',
+                        'email': 'Name1.Surname1@example.com',
+                        'forename': 'Name1',
+                        'role': 'NGI Operations Manager',
+                        'surname': 'Surname1'
+                    },
+                    {
+                        'certdn': '/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=Name2/CN=111111/CN=Name2 Surname2',
+                        'email': 'Name2.Surname2@example.com',
+                        'forename': 'Name2',
+                        'role': 'NGI Security Officer',
+                        'surname': 'Surname2'
+                    },
+                    {
+                        'certdn': '/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=Name3/CN=222222/CN=Name3 Surname3',
+                        'email': 'Name3.Surname3@example.com',
+                        'forename': 'Name3',
+                        'role': 'NGI Operations Deputy Manager',
+                        'surname': 'Surname3'
+                    }
+                ]
+            }
+        )
 
 
 class ParseSitesContactTest(unittest.TestCase):
@@ -54,7 +93,6 @@ class ParseSitesContactTest(unittest.TestCase):
                     }
                 ],
             }
-
         )
 
 if __name__ == '__main__':
