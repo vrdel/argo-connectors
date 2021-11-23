7d7e28dae2becdf0831af751bbc1d1f33f49819c
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-11-23 22:51:44 +0100

    for downtimes connectors use parse methods from base

--- modules/parse/gocdb_downtimes.py
+++ modules/parse/gocdb_downtimes.py
@@ -4,36 +4,16 @@ import xml.dom.minidom
 from xml.parsers.expat import ExpatError
 from argo_egi_connectors.utils import module_class_name
 from argo_egi_connectors.exceptions import ConnectorParseError
+from argo_egi_connectors.parse.base import ParseHelpers
 
 
-class ParseDowntimes(object):
+class ParseDowntimes(ParseHelpers):
     def __init__(self, logger, data, start, end, uid=False):
         self.uid = uid
         self.start = start
         self.end = end
         self.logger = logger
         self.data = self._parse_xml(data)
-
-    def _get_text(self, nodelist):
-        node_collect = []
-        for node in nodelist:
-            if node.nodeType == node.TEXT_NODE:
-                node_collect.append(node.data)
-        return ''.join(node_collect)
-
-    def _parse_xml(self, data):
-        try:
-            return xml.dom.minidom.parseString(data)
-
-        except ExpatError as exc:
-            msg = '{} Customer:{} : Error parsing XML feed - {}'.format(module_class_name(self), self.logger.customer, repr(exc))
-            self.logger.error(msg)
-            raise ConnectorParseError()
-
-        except Exception as exc:
-            msg = '{} Customer:{} : Error - {}'.format(module_class_name(self), self.logger.customer, repr(exc))
-            self.logger.error(msg)
-            raise exc
 
     def get_data(self):
         filtered_downtimes = list()
@@ -42,13 +22,13 @@ class ParseDowntimes(object):
         try:
             for downtime in downtimes:
                 classification = downtime.getAttributeNode('CLASSIFICATION').nodeValue
-                hostname = self._get_text(downtime.getElementsByTagName('HOSTNAME')[0].childNodes)
-                service_type = self._get_text(downtime.getElementsByTagName('SERVICE_TYPE')[0].childNodes)
-                start_str = self._get_text(downtime.getElementsByTagName('FORMATED_START_DATE')[0].childNodes)
-                end_str = self._get_text(downtime.getElementsByTagName('FORMATED_END_DATE')[0].childNodes)
-                severity = self._get_text(downtime.getElementsByTagName('SEVERITY')[0].childNodes)
+                hostname = self._parse_xmltext(downtime.getElementsByTagName('HOSTNAME')[0].childNodes)
+                service_type = self._parse_xmltext(downtime.getElementsByTagName('SERVICE_TYPE')[0].childNodes)
+                start_str = self._parse_xmltext(downtime.getElementsByTagName('FORMATED_START_DATE')[0].childNodes)
+                end_str = self._parse_xmltext(downtime.getElementsByTagName('FORMATED_END_DATE')[0].childNodes)
+                severity = self._parse_xmltext(downtime.getElementsByTagName('SEVERITY')[0].childNodes)
                 try:
-                    service_id = self._get_text(downtime.getElementsByTagName('PRIMARY_KEY')[0].childNodes)
+                    service_id = self._parse_xmltext(downtime.getElementsByTagName('PRIMARY_KEY')[0].childNodes)
                 except IndexError:
                     service_id = downtime.getAttribute('PRIMARY_KEY')
                 start_time = datetime.datetime.strptime(start_str, "%Y-%m-%d %H:%M")
@@ -72,6 +52,6 @@ class ParseDowntimes(object):
 
             return filtered_downtimes
 
-        except (KeyError, IndexError, AttributeError, TypeError, AssertionError) as exc:
+        except (KeyError, IndexError, AttributeError, TypeError, AssertionError, ExpatError) as exc:
             self.logger.error(module_class_name(self) + 'Customer:%s : Error parsing feed - %s' % (self.logger.customer, repr(exc).replace('\'', '')))
-            return []
+            raise ConnectorParseError
