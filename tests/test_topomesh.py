f50c8ea26f5378776570b205bafe8a1d08bd058e
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-12-04 00:25:53 +0100

    test helper methods and mocked data for storage element path extract

--- tests/test_topomesh.py
+++ tests/test_topomesh.py
@@ -6,6 +6,8 @@ from argo_egi_connectors.parse.gocdb_contacts import ParseSiteContacts, ParseSit
     ParseServiceGroupRoles, ParseServiceGroupWithContacts, ConnectorParseError
 from argo_egi_connectors.parse.gocdb_topology import ParseServiceEndpoints
 
+from bonsai import LDAPEntry
+
 
 logger = Logger('test_contactfeed.py')
 CUSTOMER_NAME = 'CUSTOMERFOO'
@@ -14,34 +16,70 @@ CUSTOMER_NAME = 'CUSTOMERFOO'
 class MeshSePathAndTopodata(unittest.TestCase):
     def setUp(self):
         logger.customer = CUSTOMER_NAME
-        self.sample_sepath_ldap = [
+        self.sample_ldap = [
             {
-                'GlueVOInfoAccessControlBaseRule': ['VO:vo.southgrid.ac.uk'],
-                'GlueVOInfoPath': ['/dpm/phy.bris.ac.uk/home/vo.southgrid.ac.uk'],
-                'dn': '<LDAPDN GlueVOInfoLocalID=vo.southgrid.ac.uk:hdfs_pool,GlueSALocalID=hdfs_pool:replica:online,GlueSEUniqueID=lcgse01.phy.bris.ac.uk,Mds-Vo-name=UKI-SOUTHGRID-BRIS-HEP,Mds-Vo-name=local,o=grid>'
+                'GlueVOInfoAccessControlBaseRule': ['VO:ukqcd.vo.gridpp.ac.uk'],
+                'GlueVOInfoPath': ['/dpm/gla.scotgrid.ac.uk/home/ukqcd.vo.gridpp.ac.uk'],
+                'dn': 'GlueVOInfoLocalID=ukqcd.vo.gridpp.ac.uk:generalPool,GlueSALocalID=generalPool:replica:online,GlueSEUniqueID=svr018.gla.scotgrid.ac.uk,Mds-Vo-name=UKI-SCOTGRID-GLASGOW,Mds-Vo-name=local,o=grid'
             },
             {
-                'GlueVOInfoAccessControlBaseRule': ['VO:gridpp'],
-                'GlueVOInfoPath': ['/dpm/phy.bris.ac.uk/home/gridpp'],
-                'dn': '<LDAPDN GlueVOInfoLocalID=gridpp:hdfs_pool,GlueSALocalID=hdfs_pool:replica:online,GlueSEUniqueID=lcgse01.phy.bris.ac.uk,Mds-Vo-name=UKI-SOUTHGRID-BRIS-HEP,Mds-Vo-name=local,o=grid>'
+                'GlueVOInfoAccessControlBaseRule': ['VO:atlas'],
+                'GlueVOInfoPath': ['/atlas'],
+                'dn': 'GlueVOInfoLocalID=atlas,GlueSALocalID=atlas:ATLASLOCALGROUPDISK,GlueSEUniqueID=lcg-se1.sfu.computecanada.ca,Mds-Vo-name=CA-SFU-T2,Mds-Vo-name=local,o=grid'
             }
         ]
         self.sample_gridftp_endpoints = [
             {
-                'group': 'UKI-SOUTHGRID-BRIS-HEP',
-                'hostname': 'lcgse01.phy.bris.ac.uk',
-                'service': 'webdav',
-                'tags': {'info_URL': 'https://lcgse01.phy.bris.ac.uk/dpm/phy.bris.ac.uk/home/ops/',
-                        'monitored': '0',
-                        'production': '0',
-                        'scope': 'EGI, wlcg, tier2, atlas, cms, lhcb'},
-                'type': 'SITES'
+                "type": "SITES", "group": "UKI-SCOTGRID-GLASGOW", "service":
+                "eu.egi.storage.accounting", "hostname":
+                "svr018.gla.scotgrid.ac.uk", "notifications": None, "tags":
+                {
+                    "scope": "EGI, wlcg, tier2, atlas, cms, lhcb",
+                    "monitored": "1",
+                    "production": "1"
+                }
+            },
+            {
+                "type": "SITES", "group": "CA-WATERLOO-T2", "service": "SRM",
+                "hostname": "lcg-se1.uw.computecanada.ca", "notifications": None,
+                "tags": {
+                    "scope": "EGI",
+                    "monitored": "1",
+                    "production": "1",
+                    "info_URL": "httpg://lcg-se1.uw.computecanada.ca:8443/srm/managerv2",
+                    "info_SRM_port": "8443"
+                }
             }
         ]
+        self.construct_ldap_entries()
 
+
+    def construct_ldap_entries(self):
+        tmp = list()
+        for entry in self.sample_ldap:
+            new_entry = LDAPEntry(entry['dn'])
+            for key, value in entry.items():
+                if key == 'dn':
+                    continue
+                new_entry[key] = value
+            tmp.append(new_entry)
+        self.sample_ldap = tmp
+
+    def extract_value(self, key, entry):
+        if isinstance(entry, tuple):
+            for e in entry:
+                k, v = e[0]
+                if key == k:
+                    return v
+        else:
+            return entry[key]
 
     def test_meshSePathTopo(self):
-        pass
+        for entry in self.sample_ldap:
+            voname = self.extract_value('GlueVOInfoAccessControlBaseRule', entry)
+            vopath = self.extract_value('GlueVOInfoPath', entry)
+            endpoint = self.extract_value('GlueSEUniqueID', entry['dn'].rdns)
+            import ipdb; ipdb.set_trace()
 
 
 if __name__ == '__main__':
