7b5a0d903c45139983b07606534fff92e57602fc
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-01-10 22:27:38 +0100

    adapt test bootstrap for testing of coroutines

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -1,11 +1,35 @@
 from argo_egi_connectors.io.http import SessionWithRetry
-from aiohttp.test_utils import AioHTTPTestCase
+from argo_egi_connectors.log import Logger
+
 from aiohttp import web
+from functools import wraps
+
+import asyncio
+import unittest
+
+logger = Logger('test_topofeed.py')
+CUSTOMER_NAME = 'CUSTOMERFOO'
 
 
-class ConnectorsHttpRetry(AioHTTPTestCase):
+def async_test(f):
+    """
+    Decorator to create asyncio context for asyncio methods or functions.
+    """
+    @wraps(f)
+    def g(*args, **kwargs):
+        args[0].loop.run_until_complete(f(*args, **kwargs))
+    return g
+
+
+class ConnectorsHttpRetry(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # you probably have some existing code above here
+        cls.loop = asyncio.get_event_loop()
+
     def setUp(self):
-        globopts = {
+        logger.customer = CUSTOMER_NAME
+        self.globopts = {
             'authenticationcafile': '/etc/pki/tls/certs/ca-bundle.crt',
             'authenticationcapath': '/etc/grid-security/certificates',
             'authenticationhostcert': '/etc/grid-security/hostcert.pem',
@@ -21,19 +45,19 @@ class ConnectorsHttpRetry(AioHTTPTestCase):
             '/var/lib/argo-connectors/states/', 'outputweights':
             'weights_DATE.avro', 'webapihost': 'api.devel.argo.grnet.gr'
         }
+        async def setsession():
+            self.session = SessionWithRetry(logger, 'test_retry.py', self.globopts)
 
-        self.session = SessionWithRetry(logger, os.path.basename(sys.argv[0]),
-                                        globopts, custauth=auth_opts)
-        self.feed_parts = dict(
-            scheme='https',
-            netloc='operations-portal.egi.eu',
-            path='/vapor/downloadLavoisier/option/json/view/VAPOR_Ngi_Sites_Info'
-        )
+        self.loop.run_until_complete(setsession())
 
-        res = await session.http_get('{}://{}{}'.format(feed_parts.scheme,
-                                                        feed_parts.netloc,
-                                                        feed_parts.path))
+    @async_test
+    async def test_WeightsRetry(self):
+        path='/vapor/downloadLavoisier/option/json/view/VAPOR_Ngi_Sites_Info'
+        res = await self.session.http_get('{}://{}{}'.format('http', 'localhost', path))
 
-    def test_WeightsRetry(self):
-        pass
+    def tearDown(self):
+        async def run():
+            await self.session.close()
+        self.loop.run_until_complete(run())
+        self.loop.close()
 
