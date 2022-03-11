cff3c3181907b13538fee6e4af89f0e5fcb2b040
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-03-11 16:14:08 +0100

    have internal structures as lists

--- modules/parse/eoscprovider_topology.py
+++ modules/parse/eoscprovider_topology.py
@@ -9,24 +9,21 @@ class ParseResources(ParseHelpers):
         super(ParseResources, self).__init__(logger)
         self.data = data
         self.custname = custname
-        self._resources = dict()
+        self._resources = list()
         self._parse_data()
 
     def _parse_data(self):
         json_data = self.parse_json(self.data)
         for resource in json_data['results']:
-            resource_id = resource['id']
-            if resource_id not in self._resources:
-                self._resources[resource_id] = {
-                    'name': resource['name'],
-                    'provider': resource['resourceOrganisation'],
-                    'webpage': resource['webpage'],
-                    'scope': resource['tags'],
-                    'description': resource['description']
-                }
-
-    def get_group_endpoints(self):
-        return self._resources
+            self._resources.append({
+                'id': resource['id'],
+                'name': resource['name'],
+                'provider': resource['resourceOrganisation'],
+                'webpage': resource['webpage'],
+                'scope': resource['tags'],
+                'description': resource['description']
+            })
+        self.data = self._resources
 
 
 class ParseProviders(ParseHelpers):
@@ -34,24 +31,21 @@ class ParseProviders(ParseHelpers):
         super(ParseProviders, self).__init__(logger)
         self.data = data
         self.custname = custname
-        self._providers = dict()
+        self._providers = list()
         self._parse_data()
 
     def _parse_data(self):
         json_data = self.parse_json(self.data)
         for provider in json_data['results']:
-            provider_id = provider['id']
-            if provider_id not in self._providers:
-                self._providers[provider_id] = {
-                    'resources': list(),
-                    'website': provider['website'],
-                    'name': provider['name'],
-                    'abbr': provider['abbreviation'],
-                    'scope': provider['tags']
-                }
-
-    def get_group_groups(self):
-        return self._providers
+            self._providers.append({
+                'id': provider['id'],
+                'resources': list(),
+                'website': provider['website'],
+                'name': provider['name'],
+                'abbr': provider['abbreviation'],
+                'scope': provider['tags']
+            })
+        self.data = self._providers
 
 
 class ParseTopo(object):
@@ -60,6 +54,14 @@ class ParseTopo(object):
         self.resources = ParseResources(logger, resources, custname)
 
     def get_group_groups(self):
+        gg = list()
+        for pid, pattr in self.providers.data.items():
+            gge = dict()
+            gge['type'] = 'PROJECT'
+            gge['group'] = pattr['abbr']
+            gge['tags'] = pattr['scope']
+            pass
+
         return self.providers.get_group_groups()
 
     def get_group_endpoints(self):
