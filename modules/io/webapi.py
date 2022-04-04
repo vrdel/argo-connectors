79dc32e5e0c67b9c57bcf1c1b61ac7ddc27896d7
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-04-04 11:52:41 +0200

    add new connector in WebAPI initialization

--- modules/io/webapi.py
+++ modules/io/webapi.py
@@ -11,6 +11,7 @@ class WebAPI(object):
         'downtimes-gocdb-connector.py': 'downtimes',
         'topology-gocdb-connector.py': 'topology',
         'topology-csv-connector.py': 'topology',
+        'topology-eoscprovider-connector.py': 'topology',
         'topology-json-connector.py': 'topology',
         'weights-vapor-connector.py': 'weights'
     }
