39d7a168afa26db918e4928379ea26f481a46715
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-04-20 18:04:07 +0200

    initial sample and test service type GOCDB feed parse

---
+++ tests/test_servicetypefeed.py
@@ -0,0 +1,18 @@
+import unittest
+
+from argo_egi_connectors.log import Logger
+from argo_egi_connectors.exceptions import ConnectorParseError
+from argo_egi_connectors.parse.gocdb_servicetypes import ParseGocdbServiceTypes
+
+logger = Logger('test_servicetypefeed.py')
+CUSTOMER_NAME = 'CUSTOMERFOO'
+
+class ParseGocdb(unittest.TestCase):
+    def setUp(self):
+        with open('tests/sample-service_types_gocdb.xml', encoding='utf-8') as feed_file:
+            service_types = feed_file.read()
+        self.services_gocdb = ParseGocdbServiceTypes(logger, service_types)
+        self.maxDiff = None
+
+    def test_feedParse(self):
+        self.services_gocdb.get_data()
