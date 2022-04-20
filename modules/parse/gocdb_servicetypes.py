d9451141f8ea70c8d8efe628846f402a356ae62c
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-04-20 18:19:07 +0200

    ensure that new cmd runner is packaged and initial parser class

---
+++ modules/parse/gocdb_servicetypes.py
@@ -0,0 +1,13 @@
+from urllib.parse import urlparse
+from argo_egi_connectors.utils import filename_date, module_class_name
+from argo_egi_connectors.exceptions import ConnectorParseError
+from argo_egi_connectors.parse.base import ParseHelpers
+
+
+class ParseGocdbServiceTypes(ParseHelpers):
+    def __init__(self, logger, data):
+        self.data = data
+        self.logger = logger
+
+    def get_data(self):
+        pass
