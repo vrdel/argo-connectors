0f6af22d4ff7fe3fa3ece9bff4948088fdbfe85f
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-04-20 13:21:17 +0200

    add initial task gocdb_servicetypes

---
+++ modules/tasks/gocdb_servicetypes.py
@@ -0,0 +1,11 @@
+class TaskGocdbServiceTypes(object):
+    def __init__(self, logger, connector_name, globopts, webapi_opts, confcust,
+                 custname, feed):
+        self.logger = logger
+        self.connector_name = connector_name
+        self.globopts = globopts
+        self.webapi_opts = webapi_opts
+        self.confcust = confcust
+        self.custname = custname
+        self.feed = feed
+
