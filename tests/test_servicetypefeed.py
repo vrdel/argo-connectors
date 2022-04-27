542fe12f6236302605f5878d895b9476d9a93fbf
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-04-27 21:59:02 +0200

    mock logger obj and tests for failed parse

--- tests/test_servicetypefeed.py
+++ tests/test_servicetypefeed.py
@@ -1,17 +1,19 @@
 import unittest
 
+import mock
+
 from argo_egi_connectors.log import Logger
 from argo_egi_connectors.exceptions import ConnectorParseError
 from argo_egi_connectors.parse.gocdb_servicetypes import ParseGocdbServiceTypes
 from argo_egi_connectors.parse.flat_servicetypes import ParseFlatServiceTypes
 
-logger = Logger('test_servicetypefeed.py')
 CUSTOMER_NAME = 'CUSTOMERFOO'
 
 class ParseGocdb(unittest.TestCase):
     def setUp(self):
         with open('tests/sample-service_types_gocdb.xml', encoding='utf-8') as feed_file:
             service_types = feed_file.read()
+        logger = mock.Mock()
         logger.customer = CUSTOMER_NAME
         self.services_gocdb = ParseGocdbServiceTypes(logger, service_types)
         self.maxDiff = None
@@ -46,7 +48,10 @@ class ParseFlat(unittest.TestCase):
     def setUp(self):
         with open('tests/sample-topo.csv', encoding='utf-8') as feed_file:
             service_types = feed_file.read()
+        logger = mock.Mock()
         logger.customer = CUSTOMER_NAME
+        self.logger = logger
+        self.service_types = service_types
         self.services_flat = ParseFlatServiceTypes(logger, service_types, is_csv=True)
         self.maxDiff = None
 
@@ -62,3 +67,22 @@ class ParseFlat(unittest.TestCase):
                 'name': 'aai'
             }
         ])
+
+    def test_FailedFeedParse(self):
+        with self.assertRaises(ConnectorParseError) as cm:
+            self.failed_services = ParseFlatServiceTypes(self.logger,
+                                                         'RUBBISH_DATA',
+                                                         is_csv=True)
+            self.failed_services.get_data()
+        excep = cm.exception
+        self.assertTrue('CSV feed' in excep.msg)
+        self.assertTrue(CUSTOMER_NAME in excep.msg)
+
+    def test_FailedFeedParse2(self):
+        services_flat = ParseFlatServiceTypes(self.logger, self.service_types, is_csv=True)
+        services_flat.data = ['RUBBISH_DATA1', 'RUBBISH_DATA2']
+        with self.assertRaises(ConnectorParseError) as cm:
+            data = services_flat.get_data()
+        excep = cm.exception
+        self.assertTrue('CSV feed' in excep.msg)
+        self.assertTrue(CUSTOMER_NAME in excep.msg)
