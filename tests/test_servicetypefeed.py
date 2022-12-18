49be5779bed74806b17f0685a7462d5258dd77b9
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-12-18 18:44:46 +0100

    initial tests for service types WEB-API parse

--- tests/test_servicetypefeed.py
+++ tests/test_servicetypefeed.py
@@ -4,11 +4,106 @@ import mock
 
 from argo_connectors.log import Logger
 from argo_connectors.exceptions import ConnectorParseError
-from argo_connectors.parse.gocdb_servicetypes import ParseGocdbServiceTypes
+from argo_connectors.parse.webapi_servicetypes import ParseWebApiServiceTypes
 from argo_connectors.parse.flat_servicetypes import ParseFlatServiceTypes
 from argo_connectors.parse.base import ParseHelpers
 
 CUSTOMER_NAME = 'CUSTOMERFOO'
+
+
+class ParseWebApi(unittest.TestCase):
+    def setUp(self):
+        with open('tests/sample-service_types_webapi.json', encoding='utf-8') as feed_file:
+            service_types = feed_file.read()
+        logger = mock.Mock()
+        logger.customer = CUSTOMER_NAME
+        self.maxDiff = None
+        self.services_webapi = ParseWebApiServiceTypes(logger, service_types)
+        # self.fail_services_webapi = ParseWebApiServiceTypes(logger, 'FAILED_DATA')
+
+    def test_WebApiFeedParse(self):
+        service_types = self.services_webapi.get_data()
+        self.assertEqual(service_types,
+            [
+                {
+                    'description': '[Central service] gLite metadata catalogue. This service '
+                            'only needs to be installed on the request of a VO. ',
+                    'name': 'AMGA',
+                    'tags': ['topology']
+                },
+                {
+                    'description': '[Site service] This is a "dummy" Service Type to enable the '
+                            'monitoring tests for APEL accounting. All EGEE sites must '
+                            'have one instance of this Service Type, associated with a '
+                            'CE. ',
+                    'name': 'APEL',
+                    'tags': ['topology']
+                },
+                {
+                    'description': '[Site service] The Compute Element within the ARC middleware '
+                            'stack. ',
+                    'name': 'ARC-CE',
+                    'tags': ['topology']
+                },
+                {
+                    'description': '[Site service] The LCG Compute Element. Currently the '
+                            'standard CE within the gLite middleware stack. Soon to be '
+                            'replaced by the CREAM CE. ',
+                    'name': 'CE',
+                    'tags': ['topology']
+                },
+                {
+                    'description': '[Site service] The CREAM Compute Element is the new CE '
+                            'within the gLite middleware stack. ',
+                    'name': 'CREAM-CE',
+                    'tags': ['topology']
+                },
+                {
+                    'description': 'FroNTier N-Tier data distribution system. '
+                            'http://frontier.cern.ch/',
+                    'name': 'CUSTOM.ch.cern.frontier.FroNTier',
+                    'tags': ['topology']
+                },
+                {
+                    'description': 'Service for collecting accounting data from NGI_PL grid ',
+                    'name': 'CUSTOM.pl.plgrid.BAT.agent',
+                    'tags': ['topology']
+                },
+                {
+                    'description': 'Bazaar Site Admin Toolkit from NGI_PL grid',
+                    'name': 'CUSTOM.pl.plgrid.BazaarSAT',
+                    'tags': ['topology']
+                },
+                {
+                    'description': 'Generic request tracker',
+                    'name': 'CUSTOM.RequestTracker',
+                    'tags': ['topology']
+                },
+                {
+                    'description': 'Generic user portal ',
+                    'name': 'CUSTOM.UserPortal',
+                    'tags': ['topology']
+                },
+                {
+                    'description': 'Service type created from POEM',
+                    'name': 'Service.Type.One',
+                    'tags': ['poem']
+                },
+                {
+                    'description': 'Service type created from POEM',
+                    'name': 'Service.Type.Two',
+                    'tags': ['poem']
+                }
+            ]
+        )
+
+    # def test_FailedWebApiFeedParse(self):
+        # with self.assertRaises(ConnectorParseError) as cm:
+            # failed_service_types = self.fail_services_webapi.get_data()
+
+        # excep = cm.exception
+        # self.assertTrue('XML feed' in excep.msg)
+        # self.assertTrue('syntax error' in excep.msg)
 
 
 class ParseGocdb(unittest.TestCase):
