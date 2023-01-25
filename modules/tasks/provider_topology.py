ba24d7e052e66e0be60974e99f1d40898d62a5bb
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-01-25 10:42:19 +0100

    handle KeyError exception in provider find_next_paging_cursor_count()

--- modules/tasks/provider_topology.py
+++ modules/tasks/provider_topology.py
@@ -30,7 +30,7 @@ class find_next_paging_cursor_count(ParseHelpers, Callable):
     def __call__(self):
         try:
             return self._parse()
-        except ConnectorParseError as exc:
+        except (ConnectorParseError, KeyError) as exc:
             self.logger.error(repr(exc))
             self.logger.error("Tried to parse (512 chars): %.512s" % ''.join(self.res.replace('\r\n', '').replace('\n', '')))
             raise exc
