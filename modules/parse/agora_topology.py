904bbb5979d0bf566f7321eb2102089bc76bead6
Dean Hudek <hudek.dean@gmail.com>
2023-03-02 11:18:25 +0100

    unidecode and function to remove_non_utf added for group_endpoints

--- modules/parse/agora_topology.py
+++ modules/parse/agora_topology.py
@@ -3,6 +3,14 @@ from argo_connectors.utils import module_class_name
 
 import json
 from json.decoder import JSONDecodeError
+import re
+from unidecode import unidecode
+
+
+def remove_non_utf(data):
+    pattern = r"[\s+@]+"
+
+    return re.sub(pattern, "-", data)
 
 
 class ParseAgoraTopo(object):
@@ -48,58 +56,73 @@ class ParseAgoraTopo(object):
 
             for data in resources_data:
                 if data['erp_bai_providers_public'] != [] and len(data['erp_bai_providers_public']) > 1:
-                    group_name = [d['epp_bai_id']
-                                  for d in data['erp_bai_providers_public']]
+                    group_name = [d['epp_bai_id']for d in data['erp_bai_providers_public']]
+
                 elif data['erp_bai_providers_public'] != []:
                     group_name = data['erp_bai_providers_public'][0]['epp_bai_id']
+
+                data_hostname = unidecode(data["erp_bai_id"].lower().replace(" ", "_"))
+                rsc_hostname = remove_non_utf(f'agora.ni4os.eu_{data_hostname}')
+                rsc_info_id = remove_non_utf(f'{data_hostname if data["erp_bai_id"] is not None else ""}')
+                rsc_catalog_id = f'{unidecode(data["id"])}'
+                rsc_catalog_url = f'https://catalogue.ni4os.eu/?_=/resources/{unidecode(data["id"])}'
+                rsc_ext_name = f'{unidecode(data["erp_bai_name"])}'
 
                 if type(group_name) == list and group_name != []:
                     for i in range(len(group_name)):
                         resources.append({
-                            'group': group_name[i],
+                            'group': unidecode(group_name[i]),
                             'type': 'SERVICEGROUPS',
                             'service': 'catalog.service.entry',
-                            'hostname': f'agora.ni4os.eu_{data["erp_bai_id"].lower().replace(" ", "_")}',
+                            'hostname': rsc_hostname,
                             'tags': {
                                 'hostname': 'agora.ni4os.eu',
-                                'info_ID': f'{data["erp_bai_id"].lower().replace(" ", "_") if data["erp_bai_id"] is not None else ""}',
-                                'info_ext_catalog_id': f'{data["id"]}',
+                                'info_ID': rsc_info_id,
+                                'info_ext_catalog_id': rsc_catalog_id,
                                 'info_ext_catalog_type': 'resource',
-                                'info_ext_catalog_url': f'https://catalogue.ni4os.eu/?_=/resources/{data["id"]}',
-                                'info_ext_name': f'{data["erp_bai_name"]}'
+                                'info_ext_catalog_url': rsc_catalog_url,
+                                'info_ext_name': rsc_ext_name
                             }
                         })
 
                 else:
                     resources.append({
-                        'group': group_name,
+                        'group': unidecode(group_name),
                         'type': 'SERVICEGROUPS',
                         'service': 'catalog.service.entry',
-                        'hostname': f'agora.ni4os.eu_{data["erp_bai_id"].lower().replace(" ", "_")}',
+                        'hostname': rsc_hostname,
                         'tags': {
                             'hostname': 'agora.ni4os.eu',
-                            'info_ID': f'{data["erp_bai_id"].lower().replace(" ", "_") if data["erp_bai_id"] is not None else ""}',
-                            'info_ext_catalog_id': f'{data["id"]}',
+                            'info_ID': rsc_info_id,
+                            'info_ext_catalog_id': rsc_catalog_id,
                             'info_ext_catalog_type': 'resource',
-                            'info_ext_catalog_url': f'https://catalogue.ni4os.eu/?_=/resources/{data["id"]}',
-                            'info_ext_name': f'{data["erp_bai_name"]}'
+                            'info_ext_catalog_url': rsc_catalog_url,
+                            'info_ext_name': rsc_ext_name
                         }
                     })
 
             # constructed from provider entry and planted as artificial resource
             for pr_data in providers_data:
+                prov_group = unidecode(pr_data['epp_bai_id'])
+                host_pr = unidecode(pr_data["epp_bai_id"].lower().replace(" ", "_").replace(",", ""))
+                prov_hostname = remove_non_utf(f'agora.ni4os.eu_{host_pr}')
+                prov_info_id = remove_non_utf(unidecode(pr_data['epp_bai_id'].lower().replace(" ", "_").replace(",", "")))
+                prov_catalog_id = unidecode(pr_data['id'])
+                prov_catalog_url = f'https://catalogue.ni4os.eu/?_=/providers/{unidecode(pr_data["id"])}'
+                prov_ext_name = unidecode(pr_data['epp_bai_name'])
+
                 resources.append({
-                    'group': pr_data['epp_bai_id'],
+                    'group': prov_group,
                     'type': 'SERVICEGROUPS',
                     'service': 'catalog.provider.entry',
-                    'hostname': f'agora.ni4os.eu_{pr_data["epp_bai_id"].lower().replace(" ", "_").replace(",", "")}',
+                    'hostname': prov_hostname,
                     'tags': {
                         'hostname': 'agora.ni4os.eu',
-                        'info_ID': pr_data['epp_bai_id'].lower().replace(" ", "_").replace(",", ""),
-                        'info_ext_catalog_id': pr_data['id'],
+                        'info_ID': prov_info_id,
+                        'info_ext_catalog_id': prov_catalog_id,
                         'info_ext_catalog_type': 'provider',
-                        'info_ext_catalog_url': f'https://catalogue.ni4os.eu/?_=/providers/{pr_data["id"]}',
-                        'info_ext_name': pr_data['epp_bai_name']
+                        'info_ext_catalog_url': prov_catalog_url,
+                        'info_ext_name': prov_ext_name
                     }
                 })
 
