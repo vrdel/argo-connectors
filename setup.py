22879baca323d239d37ca797899272dc134159d1
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-04-28 13:07:31 +0200

    remove version extract from spec file and have in setup.py directly

--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
 from distutils.core import setup
 import glob
 
+
 NAME = 'argo-connectors'
 
 
-def get_ver():
-    try:
-        for line in open(NAME + '.spec'):
-            if "Version:" in line:
-                return line.split()[1]
-    except IOError:
-        print("Make sure that {} is in directory".format(NAME + '.spec'))
-        raise SystemExit(1)
-
-
 setup(name=NAME,
-      version=get_ver(),
+      version="2.3.0",
       author='SRCE',
       author_email='dvrcic@srce.hr',
       description='Components generate input data for ARGO Compute Engine',
@@ -23,7 +14,7 @@ setup(name=NAME,
           "Development Status :: 5 - Production/Stable",
           "License :: OSI Approved :: Apache Software License",
           "Operating System :: POSIX",
-          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.9",
           "Intended Audience :: Developers",
       ],
       url='http://argoeu.github.io/guides/sync/',
