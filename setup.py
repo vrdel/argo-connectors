0a821f54a59ba1f1a4a2bb3348b4fd818bd9dfb3
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-04-28 20:23:46 +0200

    have poetry.lock and pyproject.toml part of wheel package

--- setup.py
+++ setup.py
@@ -5,29 +5,24 @@ import glob
 NAME = 'argo-connectors'
 
 
-setup(name=NAME,
-      version="2.3.0",
-      author='SRCE',
-      author_email='dvrcic@srce.hr',
+setup(name=NAME, version="2.3.0", author='SRCE', author_email='dvrcic@srce.hr',
       description='Components generate input data for ARGO Compute Engine',
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "License :: OSI Approved :: Apache Software License",
-          "Operating System :: POSIX",
-          "Programming Language :: Python :: 3.9",
-          "Intended Audience :: Developers",
-      ],
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "License :: OSI Approved :: Apache Software License",
+                   "Operating System :: POSIX",
+                   "Programming Language :: Python :: 3.9",
+                   "Intended Audience :: Developers",
+                   ],
       url='http://argoeu.github.io/guides/sync/',
-      package_dir={'argo_connectors.io': 'modules/io',
-                   'argo_connectors.parse': 'modules/parse',
-                   'argo_connectors.mesh': 'modules/mesh',
-                   'argo_connectors.tasks': 'modules/tasks',
-                   'argo_connectors': 'modules/'},
+      package_dir={'argo_connectors.io': 'modules/io', 'argo_connectors.parse':
+                   'modules/parse', 'argo_connectors.mesh': 'modules/mesh',
+                   'argo_connectors.tasks': 'modules/tasks', 'argo_connectors':
+                   'modules/'},
       packages=['argo_connectors', 'argo_connectors.io',
                 'argo_connectors.parse', 'argo_connectors.mesh',
                 'argo_connectors.tasks'],
-      data_files=[('/etc/argo-connectors', glob.glob('etc/*.conf.template')),
-                  ('/usr/libexec/argo-connectors', [
+      data_files=[('etc/argo-connectors', glob.glob('etc/*.conf.template')),
+                  ('usr/libexec/argo-connectors', [
                       'exec/downtimes-csv-connector.py',
                       'exec/downtimes-gocdb-connector.py',
                       'exec/metricprofile-webapi-connector.py',
@@ -39,6 +34,6 @@ setup(name=NAME,
                       'exec/topology-json-connector.py',
                       'exec/topology-provider-connector.py',
                       'exec/topology-agora-connector.py',
-                      'exec/weights-vapor-connector.py',
-                  ])
-                ])
+                      'exec/weights-vapor-connector.py', ]),
+                  ('', ['poetry.lock', 'pyproject.toml'])]
+      )
