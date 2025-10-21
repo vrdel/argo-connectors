479251aef04235633079838685c4a1b9a309b48d
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-10-21 10:21:12 +0200

    refactor LDAPSessionWithRetry with Global singleton

--- modules/io/ldap.py
+++ modules/io/ldap.py
@@ -2,16 +2,18 @@ import asyncio
 
 import bonsai
 
-from argo_connectors.utils import module_class_name
+from argo_connectors.config.glob import Global
 from argo_connectors.exceptions import ConnectorHttpError
 from argo_connectors.log import Logger
+from argo_connectors.utils import module_class_name
 
 
 class LDAPSessionWithRetry:
-    def __init__(self, retry_attempts, retry_sleep, connection_timeout):
-        self.n_try = retry_attempts
-        self.retry_sleep_list = [(i + 1) * retry_sleep for i in range(retry_attempts)]
-        self.timeout = connection_timeout
+    def __init__(self):
+        self.n_try = int(Global.options()['ConnectionRetry'.lower()])
+        retry_sleep = int(Global.options()['ConnectionSleepRetry'.lower()])
+        self.retry_sleep_list = [(i + 1) * retry_sleep for i in range(self.n_try)]
+        self.timeout = int(Global.options()['ConnectionTimeout'.lower()])
 
     async def search(self, host, port, base, filter, attributes):
         raised_exc = None
