d432d81a3e0fb50df419954f837b3a71e553864a
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-11-03 16:17:37 +0100

    attach_tags in topo-flat

--- modules/tasks/flat_topology.py
+++ modules/tasks/flat_topology.py
@@ -7,12 +7,13 @@ from argo_connectors.config.customer import get_custconf
 from argo_connectors.config.glob import Global
 from argo_connectors.io.http import SessionWithRetry
 from argo_connectors.io.webapi import WebAPI
+from argo_connectors.log import Logger
 from argo_connectors.mesh.contacts import attach_contacts_topodata
+from argo_connectors.mesh.topotags import attach_tags
 from argo_connectors.parse.flat_contacts import ParseContacts
 from argo_connectors.parse.flat_topology import ParseFlatEndpoints
 from argo_connectors.tasks.common import write_state, write_topo_json as write_json
 from argo_connectors.utils import module_class_name
-from argo_connectors.log import Logger
 
 
 class TaskFlatTopology(object):
@@ -27,6 +28,8 @@ class TaskFlatTopology(object):
         self.uidservendp = self.Customer.opt('TopoUIDServiceEndpoints')
         self.is_csv = is_csv
         self.combuid = combuid
+        self.tags_ge = self.Customer.opt('TopoTagServiceEndpoints')
+        self.tags_gg = self.Customer.opt('TopoTagServiceGroups') + self.Customer.opt('TopoTagSites')
 
     def _is_feed(self, feed):
         data = urlparse(feed)
@@ -77,6 +80,12 @@ class TaskFlatTopology(object):
 
         if not self.combuid:
             await write_state(self.fixed_date, True)
+
+        if self.tags_ge or self.tags_gg:
+            group_groups, group_endpoints = await attach_tags(group_groups,
+                                                              group_endpoints,
+                                                              self.tags_gg,
+                                                              self.tags_ge)
 
         numge = len(group_endpoints)
         numgg = len(group_groups)
