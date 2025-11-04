3fa15f28fc776158aaf456fd793fd8620036f9f9
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-11-04 16:19:37 +0100

    manual set of topo type in topo-gocdb with config option

--- modules/parse/base.py
+++ modules/parse/base.py
@@ -9,11 +9,11 @@ from argo_connectors.utils import module_class_name
 
 
 class ParseHelpers():
-    def topo_type(self, ask):
+    def topo_type(self, ask, explicit_type=None):
         if self.Customer.opt('TopoSetType'):
-            topotype = [self.Customer.opt('TopoSetType')]
+            topotype = [self.Customer.opt('TopoSetType').lower()]
         else:
-            topotype = self.Customer.get_topofetchtype()
+            topotype = [ft.lower() for ft in self.Customer.get_topofetchtype()]
 
         if len(topotype) == 1:
             if 'Sites'.lower() in topotype and ask == 'gg':
@@ -24,6 +24,14 @@ class ParseHelpers():
                 return 'project'.upper()
             elif 'ServiceGroups'.lower() in topotype and ask == 'ge':
                 return 'servicegroups'.upper()
+
+        elif len(topotype) == 2 and explicit_type:
+            if explicit_type.lower() == 'Sites'.lower() and ask == 'gg':
+                return 'ngi'.upper()
+            elif explicit_type.lower() == 'ServiceGroups'.lower() and ask == 'gg':
+                return 'project'.upper()
+            elif explicit_type.lower() in ['Sites'.lower(), 'ServiceGroups'.lower()] and ask == 'ge':
+                return explicit_type.upper()
 
     def parse_extensions(self, extensions_node):
         extensions_dict = dict()
