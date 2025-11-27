ea0bd8c0d736e264456442ad847a04abb6844324
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-11-27 16:24:36 +0100

    separate task module combine_downtimes

--- exec/downtimes-combiner.py
+++ exec/downtimes-combiner.py
@@ -2,36 +2,13 @@
 
 import asyncio
 import sys
-import datetime
 
 from argo_connectors.exe.combiner import ExecCombiner
 
 from argo_connectors.exceptions import ConnectorError, ConnectorParseError, ConnectorHttpError
-from argo_connectors.tasks.gocdb_downtimes import TaskGocdbDowntimes
-from argo_connectors.tasks.flat_downtimes import TaskCsvDowntimes
-from argo_connectors.tasks.common import write_state, write_downtimes_json as write_json
 from argo_connectors.log import Logger
-from argo_connectors.io.webapi import WebAPI
-
-
-async def fetch(tasks):
-    fetched_data = await asyncio.gather(*tasks)
-    return fetched_data
-
-
-async def webapi_send(downtimes, combuid):
-    webapi = WebAPI(combuid=combuid)
-    await webapi.send(downtimes, 'downtimes')
-    await webapi.session.close()
-
-
-def combine(downtimes):
-    joint_downtimes = list()
-
-    for dt in downtimes:
-        joint_downtimes += dt
-
-    return joint_downtimes
+from argo_connectors.tasks.combine_downtimes import TaskCombineDowntimes
+from argo_connectors.tasks.common import write_state
 
 
 def main():
@@ -40,49 +17,14 @@ def main():
         exe_script=sys.argv[0],
         combiner='downtimes'
     )
-    coros = list()
-
-    current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-    # calculate start and end times
-    try:
-        start = datetime.datetime.strptime(current_date, '%Y-%m-%d')
-        end = datetime.datetime.strptime(current_date, '%Y-%m-%d')
-        timestamp = start.strftime('%Y_%m_%d')
-        start = start.replace(hour=0, minute=0, second=0)
-        end = end.replace(hour=23, minute=59, second=59)
-
-    except ValueError as exc:
-        Logger.error(exc)
-        raise SystemExit(1)
-
-    for task in combine_exec.tasks:
-        if task['type'] == 'gocdb':
-            coros.append(TaskGocdbDowntimes(start, end, current_date,
-                                            timestamp,
-                                            combuid=task['id']).run())
-        elif task['type'] == 'csv':
-            coros.append(TaskCsvDowntimes(start, end,
-                                          current_date, timestamp,
-                                          combuid=task['id']).run())
 
     try:
-        data_fetched = asyncio.run(fetch(coros))
-        downtimes = combine(data_fetched)
-        asyncio.run(write_state(None, True, task['id']))
-
-        numst = len(downtimes)
-
-        Logger.info('Customer:' + combine_exec.tenant_name + ' Joined Downtimes:%d' % (numst))
-
-        if combine_exec.globopts.options()['GeneralWriteJson'.lower()]:
-            write_json(downtimes, None, task['id'])
-
-        if combine_exec.globopts.options()['GeneralPublishWebAPI'.lower()]:
-            asyncio.run(webapi_send(downtimes, task['id']))
+        task = TaskCombineDowntimes(combine_exec)
+        asyncio.run(task.run())
 
     except (ConnectorError, ConnectorParseError, ConnectorHttpError, KeyboardInterrupt) as exc:
         Logger.error(repr(exc))
-        asyncio.run(write_state(None, False, task['id']))
+        asyncio.run(write_state(None, False, combine_exec.tasks[0]['id']))
 
 
 if __name__ == '__main__':
